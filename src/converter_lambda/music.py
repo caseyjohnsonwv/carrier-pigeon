f8efaeb5a14d4ade159cd931543c39a12507c87d
Casey Johnson <caseyjohnsonwv@gmail.com>
2022-05-27 01:41:21 -0400

    customize cache location on lambda

--- src/converter_lambda/music.py
+++ src/converter_lambda/music.py
@@ -1,13 +1,16 @@
 import applemusicpy
 import json
 import spotipy
+from spotipy import CacheFileHandler
 from spotipy.oauth2 import SpotifyClientCredentials
 import env
 
 # initialize spotify auth
+cache_path = None if __name__ == '__main__' else '/tmp/.cache'
 sp_auth_manager = SpotifyClientCredentials(
     client_id=env.SPOTIFY_CLIENT_ID,
     client_secret=env.SPOTIFY_CLIENT_SECRET,
+    cache_handler=CacheFileHandler(cache_path=cache_path)
 )
 def init_spotify():
     token = sp_auth_manager.get_access_token(as_dict=False)
@@ -32,6 +35,7 @@ def main():
 
 
 def isrc_from_track(service:str, track_id:str):
+    # get isrc for this track from its id / uri / url
     if service == 'spotify':
         sp = init_spotify()
         song = sp.track(track_id)
@@ -44,12 +48,13 @@ def isrc_from_track(service:str, track_id:str):
 
 
 def track_from_isrc(service:str, isrc:str):
+    # get this track's id on the selected service from its isrc
     if service == 'spotify':
         sp = init_spotify()
         query = f"isrc:{isrc}"
         res = sp.search(q=query, limit=1)
         if res is not None:
-            return res['tracks']['items'][0]['external_urls']['spotify']
+            return res['tracks']['items'][0]['id']
     
     elif service == 'apple':
         # TODO
