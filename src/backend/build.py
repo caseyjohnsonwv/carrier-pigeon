7b746ef0d0331ac60a3f214f40ba65cf22c195ee
Casey Johnson <caseyjohnsonwv@gmail.com>
2022-05-28 14:30:24 -0400

    build script hotfix

--- src/backend/build.py
+++ src/backend/build.py
@@ -1,18 +1,20 @@
 import logging
 import os
 from pathlib import Path
-from shutil import make_archive
+import shutil
 from subprocess import run
 from time import time
 
 
 logging.basicConfig(
     format='%(asctime)s - %(levelname)s - %(message)s',
-    level=logging.INFO,
+    level=logging.DEBUG,
 )
+logger = logging.getLogger()
 
 
 def main():
+    logger.info("Initiating build sequence")
     start = time()
     c = 0
     for dir in Path(os.getcwd()).iterdir():
@@ -20,23 +22,43 @@ def main():
             build_dir(dir)
             c += 1
     tdelta = time() - start
-    logging.info(f"Built {c} Lambdas in {tdelta:.1f} seconds")
+    logger.info(f"Built {c} Lambdas in {tdelta:.1f} seconds")
 
 
 def build_dir(dir:Path, req_file:str='requirements-lambda.txt'):
-    logging.debug(f"Building {dir.name}")
+    logger.info(f"Building {dir.name}")
     start = time()
+    # create temp dir
+    temp_dir = Path(os.getcwd()).joinpath(f"{dir.name}_build")
+    shutil.rmtree(temp_dir, ignore_errors=True)
+    os.mkdir(temp_dir)
+    # install linux requirements
     if dir.joinpath(req_file).exists():
-        logging.debug(f"Installing requirements from {req_file} to {dir.name}")
-        command = f"python -m pip install -r {dir.name}/{req_file} --no-cache-dir --upgrade --no-user --platform linux_x86_64 --only-binary=:all: --target {dir.name}"
+        logger.debug(f"Installing requirements from {dir.name}/{req_file} to {temp_dir}")
+        command = f"python -m pip install -r {dir.name}/{req_file} --no-cache-dir --upgrade --no-user --platform linux_x86_64 --only-binary=:all: --target {temp_dir}"
         exec(command)
-    make_archive(dir.name, 'zip', dir.name)
+    # copy source code to temp dir, omitting .gitignore'd files
+    files = ' '.join([f"{dir.name}/{f.name}" for f in dir.iterdir()])
+    command = f"git check-ignore {files}"
+    res = exec(command)
+    excludes = set(p.split('/')[-1] for p in res.stdout.decode('utf-8').split('\n') if len(p) > 0)
+    logger.debug(f"Excluding {len(excludes)} .gitignore'd files")
+    if len(excludes) > 0:
+         logger.debug(', '.join(excludes))
+    for f in dir.iterdir():
+        if f.name not in excludes:
+            src = f"{dir.name}/{f.name}"
+            dest = f"{temp_dir.name}/{f.name}"
+            shutil.copyfile(src, dest)
+    # zip and delete temp dir
+    shutil.make_archive(dir.name, 'zip', temp_dir.name)
+    shutil.rmtree(temp_dir)
     tdelta = time() - start
-    logging.debug(f"Built {dir.name} in {tdelta:.1f} seconds")
+    logger.info(f"Built {dir.name} in {tdelta:.1f} seconds")
 
 
 def exec(cmd:str):
-    return run(list(cmd.split(' ')), check=True)
+    return run(list(cmd.split(' ')), capture_output=True)
 
 
 if __name__ == '__main__':
